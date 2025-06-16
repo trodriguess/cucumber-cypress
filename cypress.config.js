f920d651e5d7d5c7c3cc0d85915dbf01941f7e06
Thais Rodrigues <thaiss.rodriguess@icloud.com>
2025-06-16 19:28:14 -0300

    instalando cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,25 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
+const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    specPattern: "**/*.feature",
+    baseUrl: "https://automationpratice.com.br/",
+    execTimeout: 5000,
+    setupNodeEvents(on, config) { 
+      addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      // Make sure to return the config object as it might have been modified by the plugin.
+      return config;
     },
   },
 });
